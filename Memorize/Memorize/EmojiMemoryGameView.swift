1247067f533cc57041b99b2ee8c24de0a6c4ba94
DocCodez <adielhernandez15@gmail.com>
2020-06-30 13:15:52 -0400

    Fixed up UI.

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -21,8 +21,6 @@ struct EmojiMemoryGameView: View {
         }
             .padding()
             .foregroundColor(Color.orange)
-            .font(viewModel.cards.count < 5 ? Font.largeTitle: Font.title)
-            .aspectRatio(2/3, contentMode: .fit)
     }
 }
 
@@ -30,16 +28,33 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
+            if self.card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(self.card.content)
             }
             else {
                 RoundedRectangle(cornerRadius: 10.0).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+        .aspectRatio(2/3, contentMode: .fit)
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3.0
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
 
