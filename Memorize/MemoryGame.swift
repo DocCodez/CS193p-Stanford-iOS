a5c9c3edfddf83e7478fcfc1673aaab08b1d78d1
DocCodez <adielhernandez15@gmail.com>
2020-06-29 20:47:59 -0400

    Started the Memorize app and implemented the shuffle cards.

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Adiel Hernandez on 6/29/20.
+//  Copyright © 2020 Adiel Hernandez. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
