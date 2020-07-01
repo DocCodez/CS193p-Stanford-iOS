ede2f534abad931908500ad07e89e897b1b81cdf
DocCodez <adielhernandez15@gmail.com>
2020-07-01 03:57:55 -0400

    Added cardify and pie views.

--- Memorize/Memorize/Views/EmojiMemoryGameView.swift
+++ Memorize/Memorize/Views/EmojiMemoryGameView.swift
@@ -32,20 +32,18 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     private func body(for size: CGSize) -> some View {
-        ZStack {
-            if self.card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+        if card.isFaceUp || !card.isMatched {
+            ZStack {
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                    .padding(5)
+                    .opacity(0.40)
                 Text(self.card.content)
+                    .font(Font.system(size: fontSize(for: size)))
             }
-            else {
-                if !card.isMatched {
-                    RoundedRectangle(cornerRadius: 10.0).fill()
-                }
-            }
+            .cardify(isFaceUp: card.isFaceUp)
         }
-        .font(Font.system(size: fontSize(for: size)))
     }
     
     // MARK: - Drawing Constants
@@ -54,13 +52,15 @@ struct CardView: View {
     private let edgeLineWidth: CGFloat = 3.0
     
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
